069560d8c4671dd57591806f307ecd787d08b50b
AnnieMeoww <annie.graham955@gmail.com>
2023-05-30 06:33:56 +0000

    add more test case

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from Binary_search import binary_search
 
+print(binary_search.binarySearch([1, 2, 3, 4, 5], 2))
+print(binary_search.binarySearch([1, 2, 3, 4, 5], 4))
+print(binary_search.binarySearch([1, 2, 3, 4, 5], 1))
+print(binary_search.binarySearch([1, 2, 3, 4, 5], 5))
 print(binary_search.binarySearch([1, 2, 3, 4, 5], 7))
